efef5859a9c36274f8317ce9a498baedde736962
Santos Zurdo <yonatanzsunun@gmail.com>
2023-01-06 12:37:43 -0600

    quitando console.log

--- javascript.js
+++ javascript.js
@@ -1,25 +1,13 @@
-
-
-
-function mayusculasCaracteres(dato){
-
+function mayusculasCaracteres(dato) {
   datosAceptados = /[a-z\s]/;
 
-console.log(dato)
+
   for (var i = 0; i < dato.length; i++) {
-
-if(datosAceptados.test(dato[i])==false){
-return false
+    if (datosAceptados.test(dato[i]) == false) {
+      return false;
+    }
+  }
 }
-
-
-
-
-}
-     }
-
-
-
 
 function copiar() {
   var content = document.getElementById("mensajeCopiar").innerHTML;
@@ -36,19 +24,12 @@ function copiar() {
 function encriptar() {
   var contenido = document.getElementById("MensajeTextarea").value;
 
-
-
- 
   if (contenido) {
     const encriptado = new Array();
-    if(mayusculasCaracteres(contenido)==false){
+    if (mayusculasCaracteres(contenido) == false) {
       alert("solo minusculas sin acentos");
-  
-    } else{
+    } else {
       for (var i = 0; i < contenido.length; i++) {
-
-
-     
         if (contenido[i] == "e") {
           encriptado[i] = "enter";
         } else if (contenido[i] == "a") {
@@ -65,24 +46,23 @@ function encriptar() {
       }
       //array de id a ocultar
       var datos = ["imagen", "mensaje1", "mensaje2"];
-  
+
       for (var i = 0; i < datos.length; i++) {
         ocultar(datos[i]);
       }
-  
+
       //array de id a mostrar
-  
+
       var datos = ["mensajeCopiar", "copiar"];
-  
+
       for (var i = 0; i < datos.length; i++) {
         mostrar(datos[i]);
       }
       alert("Mensaje Encriptado");
-    
+
       document.getElementById("mensajeCopiar").innerHTML = encriptado.join("");
-   
     }
-   } else {
+  } else {
     alert("Escribe el mensaje para poder encriptarlo");
   }
 }
@@ -91,69 +71,57 @@ function desencriptar() {
   var contenido = document.getElementById("MensajeTextarea").value;
 
   if (contenido) {
-    if(mayusculasCaracteres(contenido)==false){
+    if (mayusculasCaracteres(contenido) == false) {
       alert("solo minusculas sin acentos");
-  
-    } else{
-
+    } else {
       var encriptado = contenido.replaceAll("enter", "e");
       encriptado = encriptado.replaceAll("imes", "i");
       encriptado = encriptado.replaceAll("ai", "a");
       encriptado = encriptado.replaceAll("ober", "o");
       encriptado = encriptado.replaceAll("ufat", "u");
-  
+
       //array de id a ocultar
       var datos = ["imagen", "mensaje1", "mensaje2"];
-  
+
       for (var i = 0; i < datos.length; i++) {
         ocultar(datos[i]);
       }
-  
+
       //array de id a mostrar
-  
+
       var datos = ["mensajeCopiar", "copiar"];
-  
+
       for (var i = 0; i < datos.length; i++) {
         mostrar(datos[i]);
       }
-  
-      
-  
+
       document.getElementById("mensajeCopiar").innerHTML = encriptado;
     }
-
   } else {
     alert("Escribe el mensaje para poder encriptarlo");
   }
 }
 
+function limpiar() {
+  document.getElementById("MensajeTextarea").value = "";
+  var datos = ["imagen", "mensaje1", "mensaje2"];
 
-function limpiar(){
+  for (var i = 0; i < datos.length; i++) {
+    mostrar(datos[i]);
+  }
 
+  //array de id a mostrar
 
-    document.getElementById("MensajeTextarea").value=""
-    var datos = ["imagen", "mensaje1", "mensaje2"];
+  var datos = ["mensajeCopiar", "copiar"];
 
-    for (var i = 0; i < datos.length; i++) {
-      mostrar(datos[i]);
-    }
-
-    //array de id a mostrar
-
-    var datos = ["mensajeCopiar", "copiar"];
-
-    for (var i = 0; i < datos.length; i++) {
-      ocultar(datos[i]);
-    }
-
+  for (var i = 0; i < datos.length; i++) {
+    ocultar(datos[i]);
+  }
 }
 
 function ocultar(datos) {
-    document.getElementById(datos).style.display = "none";
-  }
-  function mostrar(datos) {
-    document.getElementById(datos).style.display = "block";
-  }
-  
-
-  
+  document.getElementById(datos).style.display = "none";
+}
+function mostrar(datos) {
+  document.getElementById(datos).style.display = "block";
+}
